426e499c6892005dfbb7a6ce06149285a97a1237
Ernesto Vargas <vargaslo@gmail.com>
2014-01-16 22:38:52 -0600

    formatting

--- fitdata.py
+++ fitdata.py
@@ -5,10 +5,10 @@ import scipy.optimize as opt
 from matplotlib import rc
 plt.rc('text', usetex=True)
 
-# Define function used for fitting data
+
 def func(x, a, b, c):
-  y = a * np.exp(-b * x) + c
-  return y
+    y = a * np.exp(-b * x) + c
+    return y
 
 # Generate noisy data
 xdata = np.linspace(0, 4, 1000)
@@ -22,12 +22,12 @@ fitted = func(xdata, *popt)
 
 # Make a new figure and subplots
 plt.figure()
-ax1 = plt.subplot2grid((2,1), (0,0))
-ax2 = plt.subplot2grid((2,1), (1,0))
+ax1 = plt.subplot2grid((2, 1), (0, 0))
+ax2 = plt.subplot2grid((2, 1), (1, 0))
 
 # Plot data and fit in ax1
 ax1.plot(xdata, ydata, '.', color='red', label='data')
-line=r'$%s*\exp(-%s*x)+%s$' % (popt[0], popt[1], popt[2])
+line = r'$%s*\exp(-%s*x)+%s$' % (popt[0], popt[1], popt[2])
 ax1.plot(xdata, fitted, color='blue', label=line)
 
 # Plot residual in ax2
--- listfiles.py
+++ listfiles.py
@@ -12,30 +12,30 @@ def sorted_nicely(l):
 
 
 def matchingfiles(dir, string):
-  # Get all files in a given directory
-  where = dir
-  filelist = os.walk(where, followlinks=True)
+    # Get all files in a given directory
+    where = dir
+    filelist = os.walk(where, followlinks=True)
 
-  # Print a series of dashes to fit the terminal size
-  rows, columns = os.popen('stty size', 'r').read().split()
-  line = '-' * int(columns)
-  print line
+    # Print a series of dashes to fit the terminal size
+    rows, columns = os.popen('stty size', 'r').read().split()
+    line = '-' * int(columns)
+    print line
 
-  # Loop over files to see if they match the string
-  matchstring = string
-  print 'Recursive list of files in %s that contain "%s":' % (dir, string)
-  for root, dirnames, filenames in filelist:
-    matchedfiles = fnmatch.filter(filenames, matchstring)
-    matchedfiles = sorted_nicely(matchedfiles)
+    # Loop over files to see if they match the string
+    matchstring = string
+    print 'Recursive list of files in %s that contain "%s":' % (dir, string)
+    for root, dirnames, filenames in filelist:
+        matchedfiles = fnmatch.filter(filenames, matchstring)
+        matchedfiles = sorted_nicely(matchedfiles)
 
-    # If files match the string, output filenames to stdout
-    if len(matchedfiles) > 0:
-      print '\n%s/' % (root)
-      for filename in matchedfiles:
-        print filename
+        # If files match the string, output filenames to stdout
+        if len(matchedfiles) > 0:
+            print '\n%s/' % (root)
+            for filename in matchedfiles:
+                print filename
 
-  print line
-  print 'Done!\n'
+    print line
+    print 'Done!\n'
 
 
 matchingfiles('/ws/git/', '*py')
